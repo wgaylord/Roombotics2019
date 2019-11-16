281355c456e8404d7e6a7a3a9551b5647df3fb02
Emily Pang <epang@hawk.iit.edu>
2019-11-15 22:12:57 -0600

    Simplified motor code, added base mothership hailing code

--- MotorControl/MotorControl.ino
+++ MotorControl/MotorControl.ino
@@ -12,20 +12,33 @@
 //#define left 8
 //#define right 9
 
+//    ------| TOP |------
+//   |                   |
+//   -                   -
+//  LEFT               RIGHT
+//   -                   -
+//   |                   |
+//   -----| BOTTOM |------
+
 // Ticks since last measurement
 volatile int topTicks = 0;
 volatile int bottomTicks = 0;
 volatile int leftTicks = 0;
 volatile int rightTicks = 0;
 
-// Robot dimensions (in inches)
-const double yDimension = 16.75;
-const double xDimension = 23.9375;
-const double distanceFactor = 0.007955784;
+// Robot dimensions (in cm)
+const double yDimension = 42.545;
+const double xDimension = 60.80125;
+
+// Number to multiply ticks by to get distance in cm
+const double distanceFactor = 0.026943587;
+
+// Factor to get constant motor power for all motors
 const double motorFactor = 0.52466368;
 
-double currentX = xDimension;
-double currentY = yDimension;
+// Set these as the coordinates of the initial robot position (in cm)
+double currentX = 0;
+double currentY = yDimension / 2;
 
 void addTickTop() {
   topTicks++;
@@ -45,6 +58,8 @@ void addTickRight() {
 
 // Interrupt pins: 2, 3, 18, 19, 20, 21
 void setup() {
+  // Reset the gyro
+  // Reset the encoders
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
   pinMode(8,OUTPUT);
@@ -57,13 +72,12 @@ void setup() {
 }
 
 void loop() {
-
   topTicks = 0;
   bottomTicks = 0;
   leftTicks = 0;
   rightTicks = 0;
-  currentX = getXDistance();
-  currentY = getYDistance();
+  moveTo(50, 50);
+  hailMother();
 //  moveForwardBackwards(1024,100);
 //  delay(1000);
 //  moveLeftRight(1024,100);
@@ -72,7 +86,6 @@ void loop() {
 //  delay(1000);
 //  moveLeftRight(1024,-100);
 //  delay(1000);
-  
 }
 
 void moveForwardBackwards(int dist, int dir) { //Distance is in mm Direction is positve is forward negative is back. Abs value of direction is speed
@@ -97,11 +110,12 @@ void moveForwardBackwards(int dist, int dir) { //Distance is in mm Direction is 
 }
 
 double getXDistance() {
-  return currentX + (topTicks + bottomTicks) * distanceFactor;  
+  // Initial x position of robot + the 
+  return currentX + ((topTicks + bottomTicks) / 2) * distanceFactor;  
 }
 
 double getYDistance() {
-  return currentY + (leftTicks + rightTicks) * distanceFactor;
+  return currentY + ((leftTicks + rightTicks) / 2) * distanceFactor;
 }
 
 double setXPosition() {
@@ -112,18 +126,38 @@ double setYPosition() {
   
 }
 
+// Position for robot to move to on the coordinate system (in cm). The origin is centered at the robot's center as specified by currentX and currentY.
 void moveTo(double x, double y) {
   double initXDistance = x - getXDistance();
   double initYDistance = y - getYDistance();
-  double xDistanceLeft, yDistanceLeft;
+  double xDistanceLeft = initXDistance;
+  double yDistanceLeft = initYDistance;
   
-  while (xDistanceLeft > 0.5 && yDistanceLeft > 0.5) {
-    analogWrite(8, abs(xDistanceLeft / initXDistance));
-    analogWrite(9, abs(xDistanceLeft / initXDistance));
-    analogWrite(6, abs(yDistanceLeft / initYDistance));
-    analogWrite(7, abs(yDistanceLeft / initYDistance));
-    xDistanceLeft = x - getXDistance();
-    yDistanceLeft = y - getYDistance();
+  if (initXDistance > 0 && initYDistance > 0) {
+    while (xDistanceLeft > 0.5 && yDistanceLeft > 0.5) {
+      // Top
+      moveMotor(7, 8, (xDistanceLeft / initXDistance) * motorFactor);
+      // Bottom
+      moveMotor(9, 10, (xDistanceLeft / initXDistance) * motorFactor);
+      // Left
+      moveMotor(3, 4, yDistanceLeft / initYDistance);
+      // Right
+      moveMotor(5, 6, yDistanceLeft / initYDistance);
+      xDistanceLeft = x - getXDistance();
+      yDistanceLeft = y - getYDistance();
+    }
+  }
+}
+
+// Move a motor at a specified velocity from -1 to 1. Negative velocity indicates backwards movement. The pwmPin should be wired positively and the ground should be wired negatively.
+void moveMotor(int pwmPin, int groundPin, double velocity) {
+  if (velocity > 0) {
+    analogWrite(pwmPin, (int)(abs(velocity) * 255));
+    digitalWrite(groundPin, LOW);   
+  }
+  else if (velocity < 0) {
+    analogWrite(groundPin, (int)(abs(velocity) * 255));
+    digitalWrite(pwmPin, LOW);
   }
 }
 
@@ -147,12 +181,35 @@ void moveLeftRight(int dist,int dir) { //Distance is in mm Direction is positve 
   digitalWrite(8,LOW);
 }
 
+void findLight() {
+  while (!isLit()) {
+    analogWrite(11, 255);
+    delay(1000);
+  }
+  analogWrite(11, 0);
+}
+
+void hitLight() {
+  analogWrite(12, 255);
+  delay(1000);
+  analogWrite(12, 0);
+}
+
 bool isLit() {
   bool isLit = false;
   if (analogRead(0) > 100) {
     isLit = true;
   }
   return isLit;
+}
+
+void hailMother() {
+  int hit = 0;
+  while (hit < 6) {
+    findLight();
+    hitLight();
+    hit++;
+  }
 }
 
 // Modify and test this
@@ -167,4 +224,3 @@ bool isLit() {
 //    _LeftEncoderTicks += _LeftEncoderBSet ? -1 : +1;
 //  #endif
 //}
-
