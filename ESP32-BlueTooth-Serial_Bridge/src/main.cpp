accf95fb4dcab80dbb977076c31bf8f8a720bee4
William Gaylord <chibill110@gmail.com>
2019-11-19 14:28:23 -0600

    Make ESP32 act just as a Serial Bridge for drive testing.
    Disable ESTOP Watchdog timer.

--- ESP32-BlueTooth-Serial_Bridge/src/main.cpp
+++ ESP32-BlueTooth-Serial_Bridge/src/main.cpp
@@ -13,27 +13,18 @@ void IRAM_ATTR resetModule() {
 void setup() {
   Serial.begin(9600);
   SerialBT.begin("RoomBotics-2019-WG"); //Bluetooth device name
-  timer = timerBegin(0, 80, true);                  //timer 0, div 80
-  timerAttachInterrupt(timer, &resetModule, true);  //attach callback
-  timerAlarmWrite(timer, 500 * 1000, false); //set time in us
-  timerAlarmEnable(timer);                          //enable interrupt
+  //timer = timerBegin(0, 80, true);                  //timer 0, div 80
+  //timerAttachInterrupt(timer, &resetModule, true);  //attach callback
+  //timerAlarmWrite(timer, 500 * 1000, false); //set time in us
+  //timerAlarmEnable(timer);                          //enable interrupt
 }
 void loop() {
  while(Serial.available()) {
     SerialBT.write(Serial.read());
  }
  if (SerialBT.available()) {
-    int type = SerialBT.read();
-    if(type == 1){
-      digitalWrite(0,HIGH);
-    }
-    if(type== 0){
-      SerialBT.println(0);
-    }else{
-    while(SerialBT.available()){
-      Serial.write(SerialBT.read());
-    }
-  }}
+   Serial.write(SerialBT.read())
+  }
   delay(20);
 }
 
