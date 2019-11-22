f9e8085a49dacafb726baba898350bb7e63a0c21
William Gaylord <chibill110@gmail.com>
2019-11-22 12:58:21 -0600

    Make ready for competition.

--- ESP32-BlueTooth-Serial_Bridge/src/main.cpp
+++ ESP32-BlueTooth-Serial_Bridge/src/main.cpp
@@ -5,6 +5,7 @@ BluetoothSerial SerialBT;
 boolean waiting = false;
 hw_timer_t *timer = NULL; //Setup watchdog for if computer stops talking for 500ms
 
+unsigned long waiting = 0;
 
 //Pin 4 on ESP is the ESTOP
 void IRAM_ATTR resetModule() {
@@ -13,35 +14,28 @@ void IRAM_ATTR resetModule() {
 }
 
 
-//Uncomment Timer code for competition
+
 void setup() {
-  Serial.begin(9600);
   SerialBT.begin("RoomBotics-2019-WG"); //Bluetooth device name
-  //timer = timerBegin(0, 80, true);                  //timer 0, div 80
-  //timerAttachInterrupt(timer, &resetModule, true);  //attach callback
-  //timerAlarmWrite(timer, 500 * 1000, false); //set time in us
-  //timerAlarmEnable(timer);                          //enable interrupt
+  timer = timerBegin(0, 80, true);                  //timer 0, div 80
+  timerAttachInterrupt(timer, &resetModule, true);  //attach callback
+  timerAlarmWrite(timer, 500 * 1000, false); //set time in us
+  timerAlarmEnable(timer);                          //enable interrupt
+  while (!SerialBT.available()) {}
+  SerialBT.read();
+  SerialBT.write(1);
+  waiting = millis();
+  digitalWrite(15,HIGH);
+  
 }
 void loop() {
- while(Serial.available()) {
-    SerialBT.write(Serial.read());
- }
+
  if (SerialBT.available()) {
-   Serial.write(SerialBT.read())
+   SerialBT.write(SerialBT.read())
   }
-  delay(20);
+  if(millis() - waiting > 122000){
+    digitalWrite(2,HIGH)
+  }
+  delay(100);
 }
 
-// Change to loop for actual competition.  Pin 15 on ESP is the start signal. Pin 2 on ESP is the ready to lift signal.
-void loop1() {
- if (SerialBT.available()) {
-   int data = SerialBT.read();
-   if(data == 'S'){
-     digitalWrite(15,HIGH);
-   }
-   if(data == 'L'){
-     digitalWrite(2,HIGH)
-   }
-   SerialBT.write('0');
-  }
-}
